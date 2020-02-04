7c3cb3ee5a9a1be4e8e6b51fe6e98144d64be086
Kavan Burgess <rc0037072@student.riversidecollege.ac.uk>
2020-02-04 12:20:54 +0000

    MOVEMENT

--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -21,5 +21,47 @@ namespace move_picture
         {
 
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X > 0)
+           {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 5);
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y - 5);
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 5);
+        }
     }
 }
